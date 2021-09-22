8620656f08b4f641d9e4dd37f95013791d72fc91
Sebastian-Q <20203tn049@utez.edu.mx>
2021-09-22 10:56:24 -0500

    primer commit

---
+++ src/main/java/com/example/Servicio_Web/Servicio.java
@@ -0,0 +1,27 @@
+package com.example.Servicio_Web;
+
+import com.sun.corba.se.spi.activation.EndPointInfo;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.ws.Endpoint;
+
+
+@WebService(name= "Servicio", targetNamespace = "utez")
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+
+public class Servicio{
+
+    @WebMethod(operationName = "devuelveMensaje")
+    public String devuelveMensaje(@WebParam String valor){
+        return "Hola"+ valor;
+    }
+
+    public static void main(String[] args){
+        Endpoint.publish("http://localhost:7777/Servicio", new Servicio());
+    }
+
+
+}
